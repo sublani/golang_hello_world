27d3ae247d4814e038f7ef17ba9340e7d02d2f98
sergonji <s>
2021-10-17 15:11:11 +0000

    Update

--- app/main.go
+++ app/main.go
@@ -1,32 +1,14 @@
 package main
 
 import (
-	"fmt"      // Imprimir en consola
-	"io"       // Ayuda a escribir en la respuesta
-	"log"      //Loguear si algo sale mal
-	"net/http" // El paquete HTTP
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-
-	http.HandleFunc("/prueba1", func(w http.ResponseWriter, peticion *http.Request) {
-		io.WriteString(w, "Solicitaste prueba1")
-	})
-
-	http.HandleFunc("/prueba2", func(w http.Response1Writer, peticion *http.Request) {
-		io.WriteString(w, "Solicitaste prueba2")
-	})
-
-	http.HandleFunc("/prueba3", func(w http.ResponseWriter, peticion *http.Request) {
-		io.WriteString(w, "Solicitaste prueba3")
-	})
-
-	http.HandleFunc("/prueba4", func(w http.ResponseWriter, peticion *http.Request) {
-		io.WriteString(w, "Solicitaste prueba4")
-	})
-
-	http.HandleFunc("/prueba5", func(w http.ResponseWriter, peticion *http.Request) {
-		io.WriteString(w, "Solicitaste prueba5")
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
 	direccion := ":8080" // Como cadena, no como entero; porque representa una dirección
